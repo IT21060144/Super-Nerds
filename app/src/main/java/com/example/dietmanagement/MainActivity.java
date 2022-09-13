952bfbe30f76c0a6358606b2c86d9f8ef6cb4faa
IT21060144 <it21060144@my.sliit.lk>
2022-09-14 01:11:57 +0530

    commit1

---
+++ app/src/main/java/com/example/dietmanagement/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.dietmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.content.Intent;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText InputName;
+    EditText InputEmail;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        InputName = findViewById(R.id.editTextTextPersonName6);
+        InputEmail = findViewById(R.id.editTextTextPersonName7);
+
+        String msg = InputName.getText().toString();
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+
+        String msg1 = InputEmail.getText().toString();
+        Toast.makeText(this, msg1, Toast.LENGTH_SHORT).show();
+    }
+
+        public void clickToViewToast1(View view){
+
+            String msg = InputName.getText().toString();
+            Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+
+
+    }
+
+    public void clickToViewToast2(View view){
+
+
+        String msg1 = InputEmail.getText().toString();
+        Toast.makeText(this, msg1, Toast.LENGTH_SHORT).show();
+    }
+
+    public void onButtonClick(View v){
+        if(v.getId() == R.id.btnProfile)
+        {
+           Intent i = new Intent(MainActivity.this,profile.class);
+           startActivity(i) ;
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/dietmanagement/profile.java
@@ -0,0 +1,14 @@
+package com.example.dietmanagement;
+
+import android.app.Activity;
+import android.app.Notification;
+import android.os.Bundle;
+
+public class profile extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile);
+
+    }
+}
